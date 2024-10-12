e4b93cf94948c5f495df88ed8cf01d3b12f68517
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-10-12 10:38:57 +0400

    first commit

---
+++ client1/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './Button'
+const App = () => {
+  return (
+    <div>
+      <h1>APP Component</h1>
+      <header>
+        <h1>asdasd</h1>
+        <Button text={"Sign Up"} />
+      </header>
+    </div>
+  )
+}
+
+export default App
