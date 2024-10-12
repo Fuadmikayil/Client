131714b8f7d2bb4c08bed3ecfefe8d31dcefd3a4
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-10-12 10:39:50 +0400

    add teg

--- client1/src/App.jsx
+++ client1/src/App.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 import Button from './Button'
 const App = () => {
   return (
-    <div>
-      <h1>APP Component</h1>
-      <header>
-        <h1>asdasd</h1>
-        <Button text={"Sign Up"} />
-      </header>
-    </div>
+   <>
+   
+   </>
   )
 }
 
